fab644cde15d49e8deeb7d41e317412e04667263
amir <bashizade.official@gmail.com>
2023-12-16 09:46:25 +0330

    fix data

--- manage_library/tailwind.config.js
+++ manage_library/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const themes = require("daisyui/src/theming/themes");
 module.exports = {
-  content: [
-      './**/templates/*.html',
-      './**/templates/**/*.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: [
+        './**/templates/*.html',
+        './**/templates/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ['light', 'dark','lemonade'],
+    },
 }
 
